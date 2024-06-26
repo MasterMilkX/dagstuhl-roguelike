4d5b18209152b89c3067135ecc74f37ce7abdb0b
MMB2 <git@mmb2.click>
2024-06-26 16:53:57 +0200

    :recycle:

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # The Dragons of Castle Dagstuhl
 A roguelike made at Dagstuhl in a day with generative art and enemy AI
 
-Made by M, David, Mathias, and Alex
+Made by M, David, Matthias, and Alex
--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-sh concat.sh
+cat utils.js > dump.js
+cat entities.js >> dump.js
+cat pcg.js >> dump.js
+cat ui.js >> dump.js
+cat llama.js >> dump.js
+cat main.js >> dump.js
 rollup dump.js --file bundled_game.js --format iife
