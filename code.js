15649c15fd12c3652885e47869ca9386cab63a1b
M Charity <mastermilkx@gmail.com>
2024-06-26 09:46:39 +0200

    add base code

---
+++ code.js
@@ -0,0 +1,179 @@
+//set up the canvas
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+canvas.width = 320;
+canvas.height = 320;
+
+var size = 16;
+
+//camera
+var camera = {
+	x : 0,
+	y : 0
+};
+
+//KEYS
+
+// directionals
+var upKey = 38;     //[Up]
+var leftKey = 37;   //[Left]
+var rightKey = 39;  //[Rigt]
+var downKey = 40;   //[Down]
+var moveKeySet = [upKey, leftKey, rightKey, downKey];
+
+// A and b
+var a_key = 90;   //[Z]
+var b_key = 88;   //[X]
+var actionKeySet = [a_key, b_key];
+
+var keys = [];
+
+
+
+//////////////////    GENERIC FUNCTIONS   ///////////////
+
+
+//checks if an element is in an array
+function inArr(arr, e){
+	if(arr.length == 0)
+		return false;
+	return arr.indexOf(e) !== -1
+}
+
+
+////////////////   KEYBOARD FUNCTIONS  //////////////////
+
+
+// key events
+var keyTick = 0;
+var kt = null; 
+
+function anyKey(){
+	return anyMoveKey() || anyActionKey();
+}
+
+//check if any directional key is held down
+function anyMoveKey(){
+	return (keys[upKey] || keys[downKey] || keys[leftKey] || keys[rightKey])
+}
+
+function anyActionKey(){
+	return (keys[a_key] || keys[b_key]);
+}
+
+
+////////////////   CAMERA FUNCTIONS   /////////////////
+
+/*  OPTIONAL IF LARGE GAME MAP
+//if within the game bounds
+function withinBounds(x,y){
+	var xBound = (x >= Math.floor(camera.x / size) - 1) && (x <= Math.floor(camera.x / size) + (canvas.width / size));
+	return xBound;
+}
+
+//have the camera follow the player
+function panCamera(){
+	camera.x = 0;
+	camera.y = 0;
+
+	if(map.length != 0 && player.x > ((map[0].length) - ((canvas.width/size)/2)))
+		camera.x = (map[0].length * size) - canvas.width;
+	else if(player.x < ((canvas.width/size)/2))
+		camera.y = 0;
+	else
+		camera.x = player.x *size - (canvas.width / 2);
+
+	if(map.length != 0 && player.y > ((map.length) - ((canvas.height/size) / 2)))
+		camera.y = (map.length * size) - canvas.height;
+	else if(player.y < ((canvas.height/size)/2))
+		camera.y = 0;
+	else
+		camera.y = player.y *size - (canvas.height / 2) + (size/2);
+
+	camera.x += cam_offset.x;
+	camera.y += cam_offset.y;
+}
+*/
+
+
+//////////////////  RENDER FUNCTIONS  ////////////////////
+
+function render(){
+	ctx.save();
+	//ctx.translate(-camera.x, -camera.y);		//camera
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//background
+	ctx.fillStyle = "#dedede";
+	ctx.fillRect(0,0,canvas.width, canvas.height);
+	
+	/*   add draw functions here  */
+	
+	ctx.restore();
+}
+
+
+
+//////////////   GAME LOOP FUNCTIONS   //////////////////
+
+//game initialization function
+function init(){
+
+}
+
+//main game loop
+function main(){
+	requestAnimationFrame(main);
+	canvas.focus();
+
+	//panCamera();
+
+	render();
+
+	//keyboard ticks
+	var akey = anyKey();
+	if(akey && kt == 0){
+		kt = setInterval(function(){keyTick+=1}, 75);
+	}else if(!akey){
+		clearInterval(kt);
+		kt = 0;
+		keyTick=0;
+	}
+
+	//debug
+	var settings = "debug here";
+
+	//document.getElementById('debug').innerHTML = settings;
+}
+
+
+/////////////////   HTML5 FUNCTIONS  //////////////////
+
+//determine if valud key to press
+document.body.addEventListener("keydown", function (e) {
+	if(inArr(moveKeySet, e.keyCode)){
+		keys[e.keyCode] = true;
+	}else if(inArr(actionKeySet, e.keyCode)){
+		keys[e.keyCode] = true;
+	}
+});
+
+//check for key released
+document.body.addEventListener("keyup", function (e) {
+	if(inArr(moveKeySet, e.keyCode)){
+		keys[e.keyCode] = false;
+	}else if(inArr(actionKeySet, e.keyCode)){
+		keys[e.keyCode] = false;
+	}
+});
+
+//prevent scrolling with the game
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if(([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1)){
+        e.preventDefault();
+    }
+}, false);
+
+
+main();
